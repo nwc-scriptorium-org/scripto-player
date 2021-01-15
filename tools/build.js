67970875927945c172a469d0a5c4529720e25ba3
Noteworthy Software, Inc <online@noteworthysoftware.com>
2021-01-15 11:21:10 -0500

    Build both min and gzip versions
    Publish the min bundle.

--- tools/build.js
+++ tools/build.js
@@ -3,18 +3,18 @@ const browserify = require('browserify');
 const gzipme = require('gzipme');
 //const argv = require('minimist')(process.argv.slice(2));
 
-function BuildBundle(outFolder) {
+function BuildBundle() {
 	//if (fs.existsSync(outFolder)) fs.rmdirSync(outFolder, { recursive: true });
 	//fs.mkdirSync(outFolder);
 
 	let b = browserify('src/index.js');
-	let rawFN = `${outFolder}/scripto.js`;
+	let rawFN = `./build/scripto-player.js`;
 	let rawOutFile = fs.createWriteStream(rawFN);
 
-	rawOutFile.on('finish',() => gzipme(rawFN, {mode:'best', overwrite: true}));
+	rawOutFile.on('finish',() => gzipme(rawFN, {mode:'best', output: `${rawFN}.gz`}));
 
 	b.transform("uglifyify", {global:true});
 	b.bundle().pipe(rawOutFile);
 }
 
-BuildBundle('./build');
+BuildBundle();
